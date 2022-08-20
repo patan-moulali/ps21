aca125ab0a568adbbb9048c40f88dee1a2049ee3
LENOVO <LENOVO@DESKTOP-JJRNDTI>
2022-08-20 22:05:22 +0530

    Add project files.

---
+++ ps21/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ps21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 50;
+            int num2 = 100;
+            int biggestnum;
+            Console.WriteLine("number 1 : " + num1);
+            Console.WriteLine("number 2 : " + num2);
+            if (num1 > num2)
+            {
+
+                biggestnum = num1;
+
+            }
+            else
+            {
+                biggestnum = num2;
+            }
+
+            Console.WriteLine("biggest number is : " + biggestnum);
+            Console.Read();
+
+        }
+
+
+
+        
+    }
+}
